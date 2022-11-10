2d8f8201b80d30671b0999189dcc6f2d973cab4e
alex <chalex2k@gmail.com>
2022-11-10 22:44:49 +0300

    task 8 sample

---
+++ src/l6/task8_sample/cmd/CmdArgsParseError.java
@@ -0,0 +1,9 @@
+package l6.task8_sample.cmd;
+
+/*
+Собственный класс исключение
+ */
+public class CmdArgsParseError extends Exception{
+}
+
+
---
+++ src/l6/task8_sample/cmd/InputArgs.java
@@ -0,0 +1,14 @@
+package l6.task8_sample.cmd;
+
+/*
+Класс для хранения пути входного и выходного файла
+ */
+public class InputArgs {
+    public String inputFile;
+    public String outputFile;
+
+    public InputArgs(String inputFile, String outputFile) {
+        this.inputFile = inputFile;
+        this.outputFile = outputFile;
+    }
+}
